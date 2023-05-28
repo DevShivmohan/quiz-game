b76290e60e03315d438b9246b72f16a520cd89a8
Shivmohan <nshivmohan08@gmail.com>
2023-05-28 16:24:47 +0530

    ExceptionGenerator/Learning-everything#33

--- samta-ai/src/main/java/com/samta/ai/config/CustomConfiguration.java
+++ samta-ai/src/main/java/com/samta/ai/config/CustomConfiguration.java
@@ -1,13 +1,28 @@
 package com.samta.ai.config;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class CustomConfiguration {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    /**
+     * API rate limiting 10 requests only for 10 hours
+     * @return
+     */
+    @Bean
+    public Bucket bucket(){
+        return Bucket4j.builder().addLimit(Bandwidth.classic(10, Refill.intervally(10, Duration.ofHours(10)))).build();
     }
 }
--- samta-ai/src/main/java/com/samta/ai/controller/QuestionController.java
+++ samta-ai/src/main/java/com/samta/ai/controller/QuestionController.java
@@ -2,7 +2,10 @@ package com.samta.ai.controller;
 
 import com.samta.ai.global.CustomException;
 import com.samta.ai.service.QuestionService;
+import io.github.bucket4j.Bucket;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,17 +16,28 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@AllArgsConstructor
 public class QuestionController {
-    @Autowired
-    private QuestionService questionService;
+    private final QuestionService questionService;
+    private final Bucket bucket;
 
+    /**
+     * It will consume 1 token in every request
+     * @return
+     * @throws CustomException
+     */
     @GetMapping("/play")
-    public ResponseEntity<?> getQuestion(){
+    public ResponseEntity<?> getQuestion() throws CustomException {
+        System.out.println("Available tokens-"+bucket.getAvailableTokens());
+        if(!bucket.tryConsume(1))
+            throw new CustomException(HttpStatus.TOO_MANY_REQUESTS.value(), "API request limit exceeded");
         return questionService.getQuestion();
     }
 
     @PostMapping("/next")
     public ResponseEntity<?> getNextQuestion(@RequestBody final Map<String, Optional<Object>> request) throws CustomException {
+        if(!bucket.tryConsume(1))
+            throw new CustomException(HttpStatus.TOO_MANY_REQUESTS.value(), "API request limit exceeded");
         return questionService.getNextQuestion(request);
     }
 }
